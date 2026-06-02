da92b0ac255e7286e4ddce32ae3872f32a7db7ec
João Feitosa <joao.georges@hotmail.com>
2026-06-02 00:48:18 -0300

    feat: configuração inicial vpc, subnets e route-table

---
+++ providers.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "fake"
+  secret_key                  = "fake"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3  = "http://localhost:4566"
+    eks = "http://localhost:4566"
+    ec2 = "http://localhost:4566"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "project_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "CIDR principal da VPC"
+}
+
+variable "vpc_additional_cidrs" {
+  type        = list(string)
+  default     = []
+  description = "Lista de CIDR's adicionais da VPC"
+}
+
+variable "public_subnets" {
+  description = "Lista de Public subnets da VPC"
+  type = list(object({
+    name              = string
+    cidr              = string
+    availability_zone = string
+  }))
+}
